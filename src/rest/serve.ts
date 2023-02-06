1bb4315260d81256329e3563c3ad78a6b9f2618f
Xie Yuheng <xyheme@gmail.com>
2023-02-06 17:15:18 +0800

    up

--- src/rest/serve.ts
+++ src/rest/serve.ts
@@ -1,27 +1,65 @@
 import Http from "node:http"
 import type { Database } from "../database"
+import { requestJson } from "../utils/requestJson"
 
 type ServeOptions = {
   hostname: string
   port: number
 }
-Request
+
 export async function serve(
   db: Database,
   options: ServeOptions,
 ): Promise<void> {
-  const server = Http.createServer((request, response) => {
-    console.log(request.method)
-    console.log(request.url)
-    console.log(request.headers)
-    console.log(request.rawHeaders)
-
-    response.statusCode = 200
-    response.setHeader("Content-Type", "text/plain")
-    response.end("Hello World")
+  const server = Http.createServer(async (request, response) => {
+    try {
+      const result = await handleRequest(request)
+      const text = JSON.stringify(result, undefined, 2)
+      response.statusCode = 200
+      response.setHeader("Content-Type", "application/json")
+      response.end(text)
+    } catch (error) {
+      const message = error instanceof Error ? error.message : "Unknown Error"
+      const result = { message }
+      const text = JSON.stringify(result, undefined, 2)
+      response.statusCode = 500
+      response.setHeader("Content-Type", "application/json")
+      response.end(text)
+    }
   })
 
   server.listen(options.port, options.hostname, () => {
     console.log(`Server running at http://${options.hostname}:${options.port}/`)
   })
+}
+
+async function handleRequest(request: Http.IncomingMessage) {
+  switch (request.method) {
+    case "GET": {
+      return {
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+      }
+    }
+
+    case "PUT":
+    case "POST":
+    case "PATCH": {
+      return {
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+        json: await requestJson(request),
+      }
+    }
+
+    default: {
+      return {
+        method: request.method,
+        url: request.url,
+        headers: request.headers,
+      }
+    }
+  }
 }
--- src/utils/requestJson.ts
+++ src/utils/requestJson.ts
@@ -1,9 +1,10 @@
 import type Http from "node:http"
+import type { Json } from "./Json"
 import { requestText } from "./requestText"
 
 export async function requestJson(
   request: Http.IncomingMessage,
-): Promise<string> {
+): Promise<Json> {
   const text = await requestText(request)
   return JSON.parse(text)
 }
