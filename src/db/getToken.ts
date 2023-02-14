d8617932814759380c718e96723bda91fd40b2a2
Xie Yuheng <xyheme@gmail.com>
2023-02-15 00:24:19 +0800

    `isValidTokenName`

--- src/db/getToken.ts
+++ src/db/getToken.ts
@@ -1,9 +1,20 @@
 import type { Database } from "../database"
+import { Unauthorized } from "../errors/Unauthorized"
 import { adminToken, Token } from "../token"
+
+function isValidTokenName(tokenName: string): boolean {
+  return !tokenName.includes(".") && !tokenName.includes("/")
+}
 
 export async function getToken(
   db: Database,
   tokenName: string,
 ): Promise<Token> {
-  return adminToken
+  if (!isValidTokenName(tokenName)) {
+    throw new Unauthorized(`[getToken] not validate token name: ${tokenName}`)
+  }
+
+  const token = adminToken
+
+  return token
 }
