789b37b9642d0a90b600b8fd86be3070e2ce31c7
Xie Yuheng <xyheme@gmail.com>
2023-02-23 17:00:18 +0800

    up

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,10 +1,13 @@
 import bcrypt from "bcrypt"
 
-async function passwordHash(password: string): Promise<string> {
+export async function passwordHash(password: string): Promise<string> {
   const saltRounds = 10
   return await bcrypt.hash(password, saltRounds)
 }
 
-async function passwordCheck(password: string, hash: string): Promise<boolean> {
+export async function passwordCheck(
+  password: string,
+  hash: string,
+): Promise<boolean> {
   return await bcrypt.compare(password, hash)
 }
