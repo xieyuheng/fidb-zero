c070ca7f5c8e34b1e1a35ef449835717919bffe8
Xie Yuheng <xyheme@gmail.com>
2023-02-23 19:51:21 +0800

    up

--- src/rest/tests/rest-password-sign-in.test.ts
+++ src/rest/tests/rest-password-sign-in.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest"
 import { prepareTestServer } from "./prepareTestServer"
 
-test("rest-password-auth", async ({ meta }) => {
+test("rest-password-sign-in", async ({ meta }) => {
   const { url } = await prepareTestServer(meta)
 
   expect // TODO
---
+++ src/rest/tests/rest-password-sign-up.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest"
+import { prepareTestServer } from "./prepareTestServer"
+
+test("rest-password-sign-up", async ({ meta }) => {
+  const { url } = await prepareTestServer(meta)
+
+  expect // TODO
+})
