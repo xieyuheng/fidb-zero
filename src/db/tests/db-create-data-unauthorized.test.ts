8ddf2fc77af41c4d6ad1300d34c0eb149eb13e79
Xie Yuheng <xyheme@gmail.com>
2023-02-23 14:13:33 +0800

    db-create-data-unauthorized.test.ts

--- src/db/tests/db-create-data-unauthorized.test.ts
+++ src/db/tests/db-create-data-unauthorized.test.ts
@@ -1,15 +1,15 @@
 import { expect, test } from "vitest"
 import * as Db from "../../db"
 import { Unauthorized } from "../../errors/Unauthorized"
-import { arrayFromAsyncIterable } from "../../utils/arrayFromAsyncIterable"
 import { prepareTestDb } from "./prepareTestDb"
 
-test("db-unauthorized", async ({ meta }) => {
+test("db-create-data-unauthorized", async ({ meta }) => {
   const db = await prepareTestDb(meta)
 
-  await expect(Db.deleteDirectory(db, "..")).rejects.toThrowError(Unauthorized)
-
   await expect(
-    arrayFromAsyncIterable(Db.listDirectoryAll(db, "..")),
+    Db.createData(db, `../users/${crypto.randomUUID()}`, {
+      username: "xieyuheng",
+      name: "Xie Yuheng",
+    }),
   ).rejects.toThrowError(Unauthorized)
 })
