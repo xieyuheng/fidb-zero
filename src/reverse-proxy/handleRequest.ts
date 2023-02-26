06e30537aa667d42f1c1c1dcc7d1c5e11ae9074a
Xie Yuheng <xyheme@gmail.com>
2023-02-27 06:47:37 +0800

    [reverse-proxy] `handleRequestProxyTarget`

--- src/reverse-proxy/handleRequest.ts
+++ src/reverse-proxy/handleRequest.ts
@@ -4,6 +4,7 @@ import type { Json } from "../utils/Json"
 import { requestKind } from "../utils/requestKind"
 import type { Context } from "./Context"
 import { handleRequestPing } from "./handleRequestPing"
+import { handleRequestProxyTarget } from "./handleRequestProxyTarget"
 
 export async function handleRequest(
   ctx: Context,
@@ -15,9 +16,9 @@ export async function handleRequest(
     return await handleRequestPing(ctx, request)
   }
 
-  // if (kind.startsWith("proxy-target")) {
-  //   return await handleRequestProxyTarget(ctx, request)
-  // }
+  if (kind.startsWith("proxy-target")) {
+    return await handleRequestProxyTarget(ctx, request)
+  }
 
   throw new Error(
     [
