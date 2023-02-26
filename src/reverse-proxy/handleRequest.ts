94d36da4cd3e9d339015cbcc40fffc436eaadddc
Xie Yuheng <xyheme@gmail.com>
2023-02-27 06:46:44 +0800

    [reverse-proxy] `handleRequestPing`

--- src/reverse-proxy/handleRequest.ts
+++ src/reverse-proxy/handleRequest.ts
@@ -3,12 +3,17 @@ import type Http from "node:http"
 import type { Json } from "../utils/Json"
 import { requestKind } from "../utils/requestKind"
 import type { Context } from "./Context"
+import { handleRequestPing } from "./handleRequestPing"
 
 export async function handleRequest(
   ctx: Context,
   request: Http.IncomingMessage,
 ): Promise<Json | Buffer | void> {
   const kind = requestKind(request)
+
+  if (kind.startsWith("ping")) {
+    return await handleRequestPing(ctx, request)
+  }
 
   // if (kind.startsWith("proxy-target")) {
   //   return await handleRequestProxyTarget(ctx, request)
