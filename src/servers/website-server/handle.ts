17733d6e5a955008014115c807441c3442fc7515
Xie Yuheng <xyheme@gmail.com>
2023-03-03 13:56:12 +0800

    [website-server] inline `handlePage` to `headle`

--- src/servers/website-server/handle.ts
+++ src/servers/website-server/handle.ts
@@ -1,9 +1,12 @@
 import type { Buffer } from "node:buffer"
 import type Http from "node:http"
+import { normalize } from "node:path"
 import { handlePreflight } from "../../server/handlePreflight"
+import { requestURL } from "../../server/requestURL"
+import { responseSend } from "../../server/responseSend"
 import type { Json } from "../../utils/Json"
 import type { Context } from "./Context"
-import { handlePage } from "./handlePage"
+import { readContent } from "./readContent"
 
 export async function handle(
   ctx: Context,
@@ -16,5 +19,48 @@ export async function handle(
     }
   }
 
-  return await handlePage(ctx, request, response)
+  const url = requestURL(request)
+
+  // NOTE `decodeURIComponent` is necessary for space.
+  const path = normalize(decodeURIComponent(url.pathname.slice(1)))
+
+  const corsHeaders = ctx.cors ? { "access-control-allow-origin": "*" } : {}
+
+  if (request.method === "GET") {
+    const content =
+      (await readContent(ctx, path)) ||
+      (ctx.rewriteNotFoundTo
+        ? await readContent(ctx, ctx.rewriteNotFoundTo)
+        : undefined)
+
+    if (content === undefined) {
+      responseSend(response, {
+        status: { code: 404 },
+        headers: {
+          ...corsHeaders,
+          connection: "close",
+        },
+      })
+
+      return
+    }
+
+    responseSend(response, {
+      status: { code: 200 },
+      headers: {
+        "content-type": content.type,
+        ...corsHeaders,
+        connection: "close",
+      },
+      body: content.buffer,
+    })
+
+    return
+  }
+
+  throw new Error(
+    [`[handle] unhandled http request`, `  method: ${request.method}`].join(
+      "\n",
+    ),
+  )
 }
