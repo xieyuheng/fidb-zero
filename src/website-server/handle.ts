4d62b5fa829510bb727e801a64c3286b951c8683
Xie Yuheng <xyheme@gmail.com>
2023-03-18 02:52:45 +0800

    Compressing HTTP requests and responses

--- src/website-server/handle.ts
+++ src/website-server/handle.ts
@@ -1,12 +1,17 @@
 import type { Buffer } from "node:buffer"
 import type Http from "node:http"
 import { normalize } from "node:path"
+import { promisify } from "node:util"
+import Zlib from "node:zlib"
 import { handlePreflight } from "../server/handlePreflight"
 import { requestURL } from "../server/requestURL"
 import { responseSend } from "../server/responseSend"
 import type { Json } from "../utils/Json"
 import type { Context } from "./Context"
 import { readContent } from "./readContent"
+
+const brotliCompress = promisify(Zlib.brotliCompress)
+const gzip = promisify(Zlib.gzip)
 
 export async function handle(
   ctx: Context,
@@ -43,6 +48,40 @@ export async function handle(
       })
 
       return
+    }
+
+    if (typeof request.headers["accept-encoding"] === "string") {
+      const encodings = request.headers["accept-encoding"].split(",")
+
+      if (encodings.find((encoding) => encoding.trim().startsWith("br"))) {
+        responseSend(response, {
+          status: { code: 200 },
+          headers: {
+            "content-type": content.type,
+            "content-encoding": "br",
+            ...corsHeaders,
+            connection: "close",
+          },
+          body: await brotliCompress(content.buffer),
+        })
+
+        return
+      }
+
+      if (encodings.find((encoding) => encoding.trim().startsWith("gzip"))) {
+        responseSend(response, {
+          status: { code: 200 },
+          headers: {
+            "content-type": content.type,
+            "content-encoding": "gzip",
+            ...corsHeaders,
+            connection: "close",
+          },
+          body: await gzip(content.buffer),
+        })
+
+        return
+      }
     }
 
     responseSend(response, {
