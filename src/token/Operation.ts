9c5c8aaf3defffeb6246e8955613ac0308d794d9
Xie Yuheng <xyheme@gmail.com>
2023-04-09 16:42:37 +0800

    up

--- src/token/Operation.ts
+++ src/token/Operation.ts
@@ -2,10 +2,10 @@ import ty, { Schema } from "@xieyuheng/ty"
 
 export type Operation = "create" | "read" | "update" | "delete"
 
-export const OperationSchema: Schema<Operation> = ty.union(
-  ty.const("create" as const),
-  ty.union(
-    ty.const("read" as const),
-    ty.union(ty.const("update" as const), ty.const("delete" as const)),
-  ),
-)
+const operations = ["create", "read", "update", "delete"]
+
+function guard(x: string): x is Operation {
+  return operations.includes(x)
+}
+
+export const OperationSchema: Schema<Operation> = ty.guard(guard)
